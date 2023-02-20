ef55524d2b592a4ecd30cee7881f10acc9d0bec0
Vinod Jatav <jatavvinod1997@gmail.com>
2023-02-20 23:39:29 +0530

    Validate Subsequence class created.

---
+++ src/ValidateSubsequence.java
@@ -0,0 +1,8 @@
+public class ValidateSubsequence {
+    public static int[] validateSubsequence(){
+        return null;
+    }
+    public static void main(String[] args){
+        System.out.println("Hello, Vinod!");
+    }
+}
