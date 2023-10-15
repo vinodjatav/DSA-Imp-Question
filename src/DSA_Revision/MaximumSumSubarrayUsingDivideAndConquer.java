a9d9ac7068c2982d8e06a20c313d56ca6c735810
Vinod Jatav <jatavvinod1997@gmail.com>
2023-10-16 00:26:28 +0530

    Maximum Sum Subarray Using Divide And Conquer

---
+++ src/DSA_Revision/MaximumSumSubarrayUsingDivideAndConquer.java
@@ -0,0 +1,42 @@
+package DSA_Revision;
+
+public class MaximumSumSubarrayUsingDivideAndConquer {
+    public static int maxCrossingSum(int[] arr, int l, int m, int r) {
+        // Include elements on left of mid.
+        int sum = 0;
+        int leftSum = Integer.MIN_VALUE;
+        for (int i = m; i >= 1; i--) {
+            sum += arr[i];
+            if (sum > leftSum)
+                leftSum = sum;
+        }
+        // Include elements on right of mid
+        sum = 0;
+        int rightSum = 0;
+        for (int i = m; i <= r; i++) {
+            sum += arr[i];
+            if (sum > rightSum)
+                rightSum = sum;
+        }
+        return Math.max(leftSum + rightSum - arr[m], Math.max(leftSum, rightSum));
+    }
+
+    public static int maximumSubarraySum(int[] arr, int l, int r) {
+        if (l > r)
+            return Integer.MIN_VALUE;
+        if (l == r)
+            return arr[l];
+        int m = (l + r) / 2;
+        return Math.max(Math.max(maximumSubarraySum(arr, l, m - 1), maximumSubarraySum(arr, m + 1, r)),
+                maxCrossingSum(arr, l, m, r));
+    }
+
+    public static void main(String[] str) {
+        System.out.println("Maximum Sum Subarray Using Divide And Conquer");
+        int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+        int n = arr.length;
+        int max_sum = MaximumSumSubarrayUsingDivideAndConquer.maximumSubarraySum(arr, 0, n - 1);
+        System.out.println("Maximum contiguous sum is " + max_sum);
+
+    }
+}
